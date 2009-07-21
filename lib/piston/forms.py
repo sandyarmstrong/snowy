17845e37a399fae5619bb581b6c690451917efca
Brad Taylor <brad@getcoded.net>
2009-07-21 16:26:40 -0400

    Update piston to 92644a459862
    This update includes fixes from Benoit Garret.

--- lib/piston/forms.py
+++ lib/piston/forms.py
@@ -1,5 +1,4 @@
-import hmac
-import base64
+import hmac, base64
 
 from django import forms
 from django.conf import settings
@@ -24,7 +23,7 @@ class ModelForm(forms.ModelForm):
 
 class OAuthAuthenticationForm(forms.Form):
     oauth_token = forms.CharField(widget=forms.HiddenInput)
-    oauth_callback = forms.URLField(widget=forms.HiddenInput)
+    oauth_callback = forms.CharField(widget=forms.HiddenInput)
     authorize_access = forms.BooleanField(required=True)
     csrf_signature = forms.CharField(widget=forms.HiddenInput)
 
