af0ef3f6d2d27076418d5c5dcfd1dcea5e92c6ce
Jeff Schroeder <jschroeder@madisontyler.com>
2010-07-23 09:55:05 -0700

    Lets try pushing the right patch for bug#614815 this time folks
    Sandy pointed out this wasn't inline with:
    http://bugzilla-attachments.gnome.org/attachment.cgi?id=166392
    
    It should be golden now.

--- views.py
+++ views.py
@@ -1,8 +1,5 @@
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
 def user_index(request, username):
-    if request.user.is_authenticated() and str(request.user) == str(username):
-        return HttpResponseRedirect(reverse("note_index", args=([username])))
-    else:
-        return HttpResponseRedirect(reverse("snowy_index"))
+    return HttpResponseRedirect(reverse("note_index", args=([username])))
